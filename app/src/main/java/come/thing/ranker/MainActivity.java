0e4f40c5ad1d764a24420cd7e4a773e161db78ed
SomeStupidIdiot <somestupididiot1@gmail.com>
2020-12-19 15:22:15 -0500

    new activities

--- app/src/main/java/come/thing/ranker/MainActivity.java
+++ app/src/main/java/come/thing/ranker/MainActivity.java
@@ -1,17 +1,24 @@
 package come.thing.ranker;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.core.ImageCapture;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageCapture imageCapture;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+    }
+    public void onCreateList(View view){
+        Intent intent = new Intent(this, EditList.class);
+        startActivity(intent);
+    }
+    public void onOpenExistingList(View view){
+        Intent intent = new Intent(this, PlayActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/come/thing/ranker/PlayActivity.java
@@ -0,0 +1,16 @@
+package come.thing.ranker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class PlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
